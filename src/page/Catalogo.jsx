7f5246ae2ea1f63774122e9ade94d0d639785e32
Maximiliano Pietkiewicz <maximilianopietkiewicz04@gmail.com>
2023-10-18 12:49:28 -0300

    cambios (barra de busqueda)

--- src/page/Catalogo.jsx
+++ src/page/Catalogo.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from "../components/Navbar"
 import { Cards } from "../components/catalogo/Cards"
-
+import { Search } from "../components/catalogo/Search"
 
 
 export const Catalogo = () => {
@@ -10,6 +10,8 @@ export const Catalogo = () => {
       <div className="d-flex flex-column">
         
           <Navbar/>
+
+          <Search/>
       
           <Cards/>
 
