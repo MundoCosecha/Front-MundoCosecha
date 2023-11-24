862c1cc589b3b24b9bfedc3e57331efed123db43
Ivan Pietkiewicz <ivanciito_999@hotmail.com>
2023-11-24 02:21:40 -0300

    Se agrrgaron un par de coasa referidas a la comentar el codigo

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -8,13 +8,19 @@ import { AuthContext } from '../Context/AuthProvider'
 
 export const Login = () => {
 
+    // se crea la funcion para navegar entre paginas
     const navigate = useNavigate();
 
+    // se obtiene el login del context
     const { login } = useContext(AuthContext)
 
+    // se crean los estados para los errores
     const [errors, setErrors] = useState(false)
-    const [error, setError] = useState(false)
 
+    // se crean los estados para los errores de Login
+    const [errorLogin, setErrorLogin] = useState(false)
+
+    // se crea el estado para el usuario
     const [user, setUser] = useState({
         email: "",
         password: ""
@@ -22,39 +28,47 @@ export const Login = () => {
 
 
 
-
-
-
+    // se crea la funcion para enviar los datos al servidor
     const handleSubmit = async (e) => {
+        // se previene el comportamiento por defecto
         e.preventDefault()
 
+        // se valida que los campos no esten vacios
         if (user.email === "" || user.password === "") {
+            // se cambia el estado del error
             setErrors(true)
-            setError(false)
+            // se cambia el estado del error de login
+            setErrorLogin(false)
             return
         } else {
+            // se cambia el estado del error
             setErrors(false)
         }
+        // se envian los datos al servidor
         const res = await fetch("http://localhost:4000/auth/inicioSesion", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ user })
+            body: JSON.stringify(user)
         })
-
+        // se valida la respuesta del servidor
         if (res === 200) {
             // se obtiene la respuesta del servidor
             const data = await res.json();
             // se envia la respuesta al context
             login(data);
+            // se navega a la pagina de inicio
             navigate("/");
         } else {
-            setError(true)
+            // se cambia el estado del error de login
+            setErrorLogin(true)
         }
     }
 
+    // se crea la funcion para cambiar el estado del usuario
     const handleChange = (e) => {
+        // se cambia el estado del usuario
         setUser({
             ...user,
             [e.target.name]: e.target.value
@@ -68,7 +82,7 @@ export const Login = () => {
                     <h1>Iniciar sesión</h1>
                     <p>¡Que bueno que estes devuelta!</p>
                     <Link to={'/'} >Volver al Incio</Link>
-                    {error && <p>Usuario o contraseña incorrectos</p>}
+                    {errorLogin && <p>El usuario o la contraseña son incorrectos</p>}
                     <form className="FromRL" id="login-form" onChange={handleChange} onSubmit={handleSubmit}>
                         <label htmlFor="email" className="sr-only" >Email</label>
                         <input type="text" name="email" id="user" placeholder="Correo electronico" />
