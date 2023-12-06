7469923c1cf8adabba1491371a9c813dda866dd4
maxipietkiewicz <maximilianopietkiewicz04@gmail.com>
2023-12-06 00:58:01 -0300

    barra de busqueda funcionando...

--- src/components/catalogo/Cards.jsx
+++ src/components/catalogo/Cards.jsx
@@ -1,27 +1,7 @@
 import React from "react";
-import { useState } from "react";
 import { Card } from "./Card";
-import catalogo from "../../helpers/catalogo.json";
-export const Cards = () => {
-  const { plantas } = JSON.parse(JSON.stringify(catalogo));
 
-  const [search, setSearch] = useState({
-    nombre: "all",
-    tipo: "all",
-    especie: "all",
-  });
-
-  const filtroPlantas = (plantas) => {
-    return plantas.filter((plantas) => {
-      return (
-        plantas.nombre.toLowerCase() === search.nombre.toLowerCase() &&
-        (plantas.especie.toLowerCase() === search.especie.toLowerCase() ||
-          search.especie === "all") &&
-        (plantas.tipo.toLowerCase() === search.tipo.toLowerCase() ||
-          search.tipo === "all")
-      );
-    });
-  };
+export const Cards = ({ plantas }) => {
   return (
     <div className="d-flex flex-wrap justify-content-around align-items-center">
       {plantas.map((planta, i) => (
